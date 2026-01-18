1da710a1494978115e162b3733f0ed526f1a64fc
Assaf Shechter <asaffshechter@Asaffs-MacBook-Pro.local>
2026-01-17 20:52:18 -0600

    moving to css

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,3 @@ import MashGame from './components/MashGame.vue'
     <MashGame/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-}
-</style>
